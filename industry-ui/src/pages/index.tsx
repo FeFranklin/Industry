f5e06b43b1e35d454a2d15cd3f6756c41eb2b496
Felipe Franklin <felipelfbezerra54@gmail.com>
2023-02-25 11:22:18 -0300

    feat: ant and highcharts example

--- industry-ui/src/pages/index.tsx
+++ industry-ui/src/pages/index.tsx
@@ -2,8 +2,19 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import Highcharts from 'highcharts'
+import {
+  HighchartsProvider, HighchartsChart, Chart, XAxis, YAxis, Title, LineSeries, PlotLine
+} from 'react-jsx-highcharts';
+import { Button, Space } from 'antd';
 
 const inter = Inter({ subsets: ['latin'] })
+
+const plotOptions = {
+  series: {
+    pointStart: 2010
+  }
+};
 
 export default function Home() {
   return (
@@ -40,82 +51,37 @@ export default function Home() {
         </div>
 
         <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
+        <HighchartsProvider Highcharts={Highcharts}>
+          <HighchartsChart plotOptions={plotOptions}>
+            <Chart />
+
+            <Title>Return on investment</Title>
+
+            <XAxis>
+              <XAxis.Title>Year</XAxis.Title>
+              
+              <PlotLine id="investment" value={2012} dashStyle="Dash" width={1} color="#666">
+                <PlotLine.Label>Series A</PlotLine.Label>
+              </PlotLine>
+            </XAxis>
+
+            <YAxis id="number">
+              <YAxis.Title>Returns (£)</YAxis.Title>
+              
+              <LineSeries id="my-series" data={[43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]} />
+            </YAxis>
+          </HighchartsChart>
+        </HighchartsProvider>
         </div>
 
         <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
+        <Space wrap>
+          <Button type="primary">Primary Button</Button>
+          <Button>Default Button</Button>
+          <Button type="dashed">Dashed Button</Button>
+          <Button type="text">Text Button</Button>
+          <Button type="link">Link Button</Button>
+        </Space>
         </div>
       </main>
     </>
